2c885462f414cfb3d44785c13df5f328c569b7c5
ryankawahara <64173139+ryankawahara@users.noreply.github.com>
2022-11-21 12:32:59 +0000

    Version 1

--- joke_generator.py
+++ joke_generator.py
@@ -1,24 +1,27 @@
 from maya import cmds
-from Qt import QtCore, QtWidgets
+
+from PySide2 import QtCore, QtWidgets
+
 import maya.OpenMayaUI as omui
-import shiboken2 as shiboken
 import sys
 import time
-
+import shiboken2 as shiboken
 
 import requests
 
-
 class MyEventHandler(QtCore.QObject):
     def __init__(self):
+        cmds.inViewMessage(hide=False)
         super(MyEventHandler, self).__init__()
-        self.count=0
+        cmds.setFocus("outlinerPanel1")
+        self.count = 1
         self.joke = None
+        
+        # first joke appears unprompted
         self.newJoke()
-        print(self.joke[0])
-        cmds.inViewMessage(amg=f'<hl>{self.joke[0]}</hl>.', pos='midCenter', dragKill=True )
-
-        self.count+=1
+        
+        cmds.inViewMessage(amg=f'<hl>{self.joke[0]}</hl>.', pos='midCenterTop', dragKill=True )
+        
 
     def newJoke(self):
         r = requests.get(
@@ -27,54 +30,52 @@ class MyEventHandler(QtCore.QObject):
         setup = joke['setup']
         delivery = joke['delivery']
         self.joke = (setup, delivery)
+
     def eventFilter(self, qobj, event):
         if event.type() == event.KeyPress:
-            print(self.count)
 
-            if self.count == 0:
-                self.newJoke()
-                # self.count+=1
-                print(self.joke[0])
-            elif self.count == 1:
-                print(self.joke[1])
-            else:
-                if self.count%2 == 0:
+            key = event.key()
+
+            if (key == QtCore.Qt.Key_Left
+                or key == QtCore.Qt.Key_Up
+                or key == QtCore.Qt.Key_Right
+                or key == QtCore.Qt.Key_Down
+            ):
+
+                if self.count == 0:
                     self.newJoke()
-                    print(self.joke[0])
+                    cmds.inViewMessage(amg=f'<hl>{self.joke[0]}</hl>.', pos='midCenterTop', dragKill=True)
+                elif self.count == 1:
+                    cmds.inViewMessage(amg=f'<hl style="color: #A2CCE0;">{self.joke[1]}</hl>.', pos='midCenter', dragKill=True)
                 else:
-                    print(self.joke[1])
-            if event.key() == QtCore.Qt.Key_Escape: # QtCore.Qt.Key_Escape is a value that equates to what the operating system passes to python from the keyboard when the escape key is pressed.
-                # Yes: Close the window
-                qobj.removeEventFilter(self)
-            # print(setup)
-            # time.sleep(5)
-            # print(delivery)
+                    if self.count%2 == 0:
+                        cmds.inViewMessage(clear='midCenter')
+                        self.newJoke()
+                        cmds.inViewMessage(amg=f'<hl>{self.joke[0]}</hl>.', pos='midCenterTop', dragKill=True)
+                    else:
+                        cmds.inViewMessage(amg=f'<hl style="color: #A2CCE0;">{self.joke[1]}</hl>.', pos='midCenter', dragKill=True)
 
-            self.count+=1
+                self.count += 1
+
+            try:
+                if (
+                        key == QtCore.Qt.Key_Escape
+                        or key == QtCore.Qt.Key_Enter
+                        or key == QtCore.Qt.Key_Return
+                        or key == 16777216
+                    ): # QtCore.Qt.Key_Escape is a value that equates to what the operating system passes to python from the keyboard when the escape key is pressed.
+                    cmds.inViewMessage(clear='midCenterTop')
+                    cmds.inViewMessage(clear='midCenter')
+                    qobj.removeEventFilter(self)
+            except AttributeError:
+                pass
         return super(MyEventHandler, self).eventFilter(qobj, event)
 
 def get_maya_main_window():
-    # store the memory address for the main maya window
     pointer = omui.MQtUtil.mainWindow()
-    # return shiboken.wrapInstance(long(pointer), QtWidgets.QWidget) # long has been dropped in python 3.0
     return shiboken.wrapInstance(int(pointer), QtWidgets.QWidget)
-
-# flag = False
-# print(joke['setup'])
-# r = requests.get('https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=twopart&contains=%3F')
-# joke = r.json()
-# print(joke['setup'])
-# setup = joke['setup']
-# delivery = joke['delivery']
 
 
 handler = MyEventHandler()
 maya_window = get_maya_main_window()
 maya_window.installEventFilter(handler)
-
-
-
-
-
-# cmds.inViewMessage(amg='<hl>Selected item is not a reference node.</hl>.', pos='midCenter', dragKill=True )
-# print(r.json())
